95a30b07d5b7f6c1ea8f15ad434c8f4ad1acea52
TiboMessiaen <tibo.messiaen@student.howest.be>
2023-04-19 16:47:08 +0200

    feat: more projects

--- Portfolio/src/routes/Learning.tsx
+++ Portfolio/src/routes/Learning.tsx
@@ -11,12 +11,25 @@ function Learning() {
         <Title title="My learning adventure"/>
 
         <div className='max-w-6xl m-auto'>
-          <SeperatorLine year="2023"/>
+          <SeperatorLine year="2020"/>
           <LearningCard side='left' title='ICT year 1' subtitle='C#, C++, arduino' content="I started programming in my 5th year of high school. We learned the activate led's with arduino and micro controllers. And we started creating our first console applications with C# to get used to the syntax."/>
 
-          <SeperatorLine year="2023"/>
+          <SeperatorLine year="2021"/>
           <LearningCard side='right' title='ICT year 2' subtitle='C#, c++, arduino' content='In our second year of ICT, we went from console applications to desktop applications. And we went in depth of the c++ of how the shift registers work.'/>
           <LearningCard side='left' title='Self Learning' subtitle='C#' content='I really enjoyed the C# programmes we made and so I started creating my own C# applications with the help of youtube tutorials. I created one big dasboard application with all the different usefull features we created over the years (sorting algorithms, random number generators, ...). This was also the year that I created my first snake and tetris desktop application.'/>
+          
+          <SeperatorLine year="2022"/>
+          <LearningCard side='right' title='1MCT' subtitle='HTML, CSS, Javascript' content='This is the first year that I started to create my own websites. In the beginning of the year I started learning HTML en CSS. Once I got used to these languages I started to add some basic javascript to the websites. This immediately made the websites more usefull.'/>
+          <LearningCard side='left' title='1MCT' subtitle='Python, arduino, SQL' content='As the year went on we even created our own backend for the website. Making us able the show different data sets from our databases on our own website.'/>
+          <LearningCard side='right' title='Project One' subtitle='HTML, CSS, Javascript, Python, SQL' content='With all this behind us, it was time to create a big project to show our teachers that we mastered everything we learned this year. This was "project one". Our first complete website website with every possible aspect of it.'/>
+          <LearningCard side='left' title='Spotify App' subtitle='C#, Xamarin' content='We had our first app to create. And I choose to recreate the spotify app with the official spotify api for developers. The app has a 2.O authentication to log you in and get all your spotify playlists and songs. You can even create, update en delete all your spotify playlist. you just cant listen to any songs in the app.'/>
+          
+          <SeperatorLine year="2023"/>
+          <LearningCard side='right' title='2MCT' subtitle='C#, Azure, Docker, Dapr' content=''/>
+          <LearningCard side='left' title='2MCT' subtitle='HTML, CSS, Javascript, Typescript, Tailwind, React' content='This portfolio website you are looking at right now'/>
+          <LearningCard side='right' title='2MCT' subtitle='React Native, TypeScript' content='I created a DartCounter app in react native. I like the dart in my free time and this project was my opportunity to create my own version of a dart app, completely to my likings.'/>
+          
+          <SeperatorLine year="NOW"/>
         </div>
     </div>
   )
--- Portfolio/src/routes/Projects.tsx
+++ Portfolio/src/routes/Projects.tsx
@@ -23,7 +23,7 @@ function Projects() {
             <div className='w-full pb-4 md:w-1/2 xl:p-12'>
               <img src={PortfolioMockup} alt="placeholder" className='w-full'/>
             </div>
-            <div className='w-full pb-12 md:w-1/2 place-self-center'>
+            <div className='w-full pb-12 md:w-1/2 place-self-center xl:p-12'>
               <ProjectInfo title="Portfolio" paragraph="I created my own website portfolio" technologies={[{title:"Front-end", technologies:["HTML","Tailwindcss","Typescript"]}]} footnote="Website:" footnoteLink={<a href="https://tibomessiaen.netlify.app/" target='_blank'>Portfolio</a>}/>
             </div>
         </div>
@@ -33,7 +33,7 @@ function Projects() {
               <video title='1VS1' className='xl:w-1/2 p-2' src={TeamProject1vs1Demo} controls>Your browser does not support the video tag.</video>
               <video title='1VS1' className='hidden xl:flex w-1/2 p-2' src={TeamProjectShuttleRunDemo} controls>Your browser does not support the video tag.</video>
             </div>
-            <div className='w-full pb-12 md:w-1/2 place-self-center'>
+            <div className='w-full pb-12 md:w-1/2 place-self-center xl:p-12'>
               <ProjectInfo title="Team project" paragraph="We created this project in team. The assignment was to create an interactive game with poles. The main goal was to make people move. With the focus on children (age 8-18). I designed and created the website, I also handled the communication with the backend." technologies={[{title:"Design", technologies:["Adobe XD","Photoshop"]},{title:"Front-end", technologies:["HTML","CSS","Javascript"]},{title:"Back-end", technologies:["HTTP","MQTT","Socket IO"]}]}/>
             </div>
         </div>
@@ -42,7 +42,7 @@ function Projects() {
             <div className='w-full pb-4 md:w-1/2 xl:p-12'>
              <video title='Shower monitor' src={ProjectOneDemo} controls>Your browser does not support the video tag.</video>
             </div>
-            <div className='w-full pb-12 md:w-1/2 place-self-center'>
+            <div className='w-full pb-12 md:w-1/2 place-self-center xl:p-12'>
               <ProjectInfo title="Project One" paragraph="Dit is project one!" technologies={[{title:"Front-end", technologies:["HTML","CSS"]},{title:"Back-end", technologies:["C#",".NET"]}]} footnote="Instructables:" footnoteLink={<a href="https://www.instructables.com/Water-shower-monitor/" target='_blank'>Water Shower Monitor</a>}/>
             </div>
         </div>
@@ -51,7 +51,7 @@ function Projects() {
             <div className='w-full pb-4 md:w-1/2 xl:p-12'>
               <img src={blackjack} alt="placeholder" className='w-full'/>
             </div>
-            <div className='w-full pb-12 md:w-1/2 place-self-center'>
+            <div className='w-full pb-12 md:w-1/2 place-self-center xl:p-12'>
               <ProjectInfo title="Blackjack" paragraph="I reacreated the popular card game blackjack. Also know as 21." technologies={[{title:"Front-end", technologies:["HTML","CSS","Javascript"]}]} footnote='Website:' footnoteLink={<a href="https://messiaentibo.github.io/Interaction-Design-Eindopdracht/" target='_blank'>Blackjack</a>}/>
             </div>
         </div>
