ebce5327716a88cb5e0972fd44df3f630926f432
TiboMessiaen <tibo.messiaen@student.howest.be>
2023-04-19 20:37:05 +0200

    feat: mobile header

--- Portfolio/src/components/Header.tsx
+++ Portfolio/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { Settings, Home } from 'lucide-react';
+import { useEffect, useState } from 'react'
+import { Home, User, Scroll, Tv2, } from 'lucide-react';
 
 import { Link } from 'react-router-dom';
 
@@ -11,6 +11,13 @@ interface Props {
 
 export default ({title}:Props) => {
   const [active, setActive] = useState('Home');
+
+  useEffect(() => {
+    if(window.location.href.includes('home')) setActive('Home');
+    if(window.location.href.includes('about')) setActive('About me');
+    if(window.location.href.includes('learning')) setActive('Learning Adventure');
+    if(window.location.href.includes('projects')) setActive('Projects');
+  }, [])
 
   const body = document.querySelector("body");
 
@@ -36,7 +43,9 @@ export default ({title}:Props) => {
 
   return (
     <header className="flex place-content-between bg-white px-4 content-center text-center place-items-center h-12 shadow-header xl:h-16 xl:px-6 2xl:px-24">
-      <Link to='/' onClick={selectHome}><h1 className='text-2xl font-medium xl:text-3xl'>{title}</h1></Link>
+      <div className='hidden sm:flex '>
+        <Link to='/' onClick={selectHome}><h1 className='text-2xl font-medium xl:text-3xl'>{title}</h1></Link>
+      </div>
       <div className='hidden sm:flex xl:text-xl'>
         <ul>
           {active === 'About me' ?
@@ -53,12 +62,24 @@ export default ({title}:Props) => {
 
         </ul>
       </div>
-      <div className="c-header__mobile-nav-trigger sm:hidden">
-        <button className="o-button-reset c-nav-trigger js-toggle-nav" onClick={handleClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 18">
-                <path className="c-nav-trigger__svg" id="navbar_mobile" d="M3,24H27V21H3v3Zm0-7.5H27v-3H3v3ZM3,6V9H27V6Z" transform="translate(-3 -6)" fill="" fill-rule="evenodd"/>
-              </svg>
-        </button>
+      <div className='flex w-full sm:hidden xl:text-xl'>
+        <ul className='flex place-content-around w-full '>
+            {active === 'Home' ?
+              <li className='inline'><Link className='p-3 xl:p-4 border-b-4 border-orange-peel' to='/'><Home className='inline p-2 stroke-orange-peel' size={50}/></Link></li> :
+              <li className='inline'><Link className='p-3 xl:p-4 hover:border-b-4 border-caribbean-current' to='/' onClick={selectHome}><Home className='inline p-2 stroke-caribbean-current' size={50}/></Link></li>}
+            {active === 'About me' ?
+              <li className='inline'><Link className='p-3 xl:p-4 border-b-4 border-orange-peel' to='/about'><User className='inline p-2 stroke-orange-peel' size={50}/></Link></li> :
+              <li className='inline'><Link className='p-3 xl:p-4 hover:border-b-4 border-caribbean-current' to='/about' onClick={selectAbout}><User className='inline p-2 stroke-caribbean-current' size={50}/></Link></li>}
+
+            {active === 'Learning Adventure' ?
+              <li className='inline'><Link className='p-3 xl:p-4 border-b-4 border-orange-peel' to='/learning'><Scroll className='inline p-2 stroke-orange-peel' size={50}/></Link></li> :
+              <li className='inline'><Link className='p-3 xl:p-4 hover:border-b-4 border-caribbean-current' to='/learning' onClick={selectLearning}><Scroll className='inline p-2 stroke-caribbean-current' size={50}/></Link></li>}
+            
+            {active === 'Projects' ?
+              <li className='inline'><Link className='p-3 xl:p-4 border-b-4 border-orange-peel' to='/projects'><Tv2 className='inline p-2 stroke-orange-peel' size={50}/></Link></li> :
+              <li className='inline'><Link className='p-3 xl:p-4 hover:border-b-4 border-caribbean-current' to='/projects' onClick={selectProjects}><Tv2 className='inline p-2 stroke-caribbean-current' size={50}/></Link></li>}
+
+          </ul>
       </div>
     </header>
   );
